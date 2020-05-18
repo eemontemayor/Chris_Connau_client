e7b6b52fc83d675d92b026e0bc5fa571b7e7ab94
eemontemayor <eemontemayor@live.com>
2020-05-18 13:38:48 -0500

    carousel receiving img array without errors

--- src/routes/ArticleListPage/ArticleListPage.js
+++ src/routes/ArticleListPage/ArticleListPage.js
@@ -1,78 +1,75 @@
-import React, { Component } from 'react'
-import ArticleListContext from '../../contexts/ArticleListContext'
-import ArticleApiService from '../../services/article-api-service'
-import CarouselApiService from '../../services/carousel-api-service'
-import { Section } from '../../components/Utils/Utils'
-import ArticleListItem from '../../components/ArticleListItem/ArticleListItem'
-import './ArticleListPage.css'
-import Carousel from '../../components/Carousel/Carousel'
+import React, { Component } from "react";
+import ArticleListContext from "../../contexts/ArticleListContext";
+import ArticleApiService from "../../services/article-api-service";
+import CarouselApiService from "../../services/carousel-api-service";
+import { Section } from "../../components/Utils/Utils";
+import ArticleListItem from "../../components/ArticleListItem/ArticleListItem";
+import "./ArticleListPage.css";
+import Carousel from "../../components/Carousel/Carousel";
 export default class ArticleListPage extends Component {
-  state={
-    images:[]
-}
+  state = {
+    images: [],
+  };
 
-
-  static contextType = ArticleListContext
+  static contextType = ArticleListContext;
 
   componentDidMount() {
-    this.context.clearError()
+    this.context.clearError();
     ArticleApiService.getArticles()
       .then(this.context.setArticleList)
-      .catch(this.context.setError)
+      .catch(this.context.setError);
 
-    CarouselApiService.getCarouselPics()
-    .then(res => console.log('res', res))
-
-
+  let imgArr = CarouselApiService.getCarouselPics()
+    console.log('img', imgArr)
+  this.setState({
+    images:imgArr
+  })
 
   }
 
   renderArticles() {
-    const { articleList = [] } = this.context
-    return articleList.map(article =>
-      <ArticleListItem
-        key={article.id}
-        article={article}
-      />
-    )
+    const { articleList = [] } = this.context;
+    return articleList.map((article) => (
+      <ArticleListItem key={article.id} article={article} />
+    ));
   }
 
-
-
-renderGalleryList(arr){
-
-  return arr.map((img, index) => {
-    return <img src={img} className='GalleryListItem' key={`img-${index}`} />;
-					})
-}
-
+  renderGalleryList(arr) {
+    return arr.map((img, index) => {
+      return <img src={img} className="GalleryListItem" key={`img-${index}`} />;
+    });
+  }
 
   render() {
-    const { error } = this.context
-    const  images = this.state.images
-     
+    const { error } = this.context;
+    const images = this.state.images;
+
     return (
-      <div  className='ArticleListPage'>
-        <div className='Carousel'>
-     
-        <Carousel images = {images}/>        </div>
-        <div 
-        // list 
-        className='ArticleListContainer' >
-      <ul className='ArticleList'>
-
-        {error
-          ? <p className='red'>There was an error, try again</p>
-          : this.renderArticles()}
+      <div className="ArticleListPage">
+        <div className="Carousel">
+          <Carousel images={images} />{" "}
+        </div>
+        <div
+          // list
+          className="ArticleListContainer"
+        >
+          <ul className="ArticleList">
+            {error ? (
+              <p className="red">There was an error, try again</p>
+            ) : (
+              this.renderArticles()
+            )}
           </ul>
 
-          <ul className ='GalleryList' >
-          {error
-          ? <p className='red'>There was an error, try again</p>
-          : this.renderGalleryList(images)}
-          </ul>
-          </div>
+          {/* <ul className="GalleryList">
+            {error ? (
+              <p className="red">There was an error, try again</p>
+            ) : (
+              this.renderGalleryList(images)
+            )}
+          </ul> */}
+        </div>
       </div>
-    )
+    );
   }
 }
--- src/services/carousel-api-service.js
+++ src/services/carousel-api-service.js
@@ -1,15 +1,21 @@
 import config from '../config'
 import TokenService from '../services/token-service'
 import first from '../images/1carousel.jpg'
+import second from '../images/2carousel.jpg'
+import third from '../images/3carousel.jpg'
+import fourth  from '../images/4carousel.jpg'
+import fifth from '../images/5carousel.jpg'
+import sixth from '../images/6carousel.jpg'
 const CarouselApiService = {
     getCarouselPics(){
         
-        const obj = {
-          img:first,
-          details:'blah blah blah'
-        }
-        console.log('obj', obj)
-        return obj
+
+      const imgArr = [
+        first,second,third,fourth,fifth,sixth
+      ]
+
+     
+        return imgArr
 
 
     },
